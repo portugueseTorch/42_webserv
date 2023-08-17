0839ee4a6da7f0a85970760b37648f148d81a1b7
TERESA OUTEIRO MARTINS MANSO <teresa.manso@mercedes-benz.io>
2023-08-17 10:28:38 +0100

    added documentation and structure

--- inc/HTTPRequest.hpp
+++ inc/HTTPRequest.hpp
@@ -8,6 +8,24 @@ enum RequestMethod {
 	POST,
 	DELETE
 };
+// Documentation: https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages
+
+// method, url, http-version
+class StartLine; 
+
+//case-insensitive string followed by a colon (':') and a value whose structure depends upon the header.
+class HTTPHeader; 
+
+//there needs to be an empty line in between
+
+//Optional: GET usually doesn't need one
+/* 
+Bodies can be broadly divided into two categories:
+
+Single-resource bodies, consisting of one single file, defined by the two headers: Content-Type and Content-Length.
+Multiple-resource bodies, consisting of a multipart body, each containing a different bit of information. This is typically associated with HTML Forms.
+ */
+class HTTPBody;
 
 class HTTPRequest {
 	public:
@@ -34,6 +52,6 @@ class HTTPRequest {
 			Message body
 		 */
 
-}
+};
 
 #endif
